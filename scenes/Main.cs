e9eb310fe3d1c80891d75c45f56f8e5dc411ee23
NhemNhem <95702943+NhomNhem@users.noreply.github.com>
2026-04-07 22:15:59 +0700

    feat: add GridManager for tile occupancy and highlight logic, refactor Main to use GridManager, and update scene files

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -1,40 +1,43 @@
+using Game.manager;
 using Godot;
 
 namespace Game;
 
 public partial class Main : Node {
+    
+    private GridManager _gridManager;
+    
     private Sprite2D _cursor;
     private PackedScene _buildingScene;
     private Button _placeBuildingButton;
-    private TileMapLayer _highTileMapLayer;
 
     private Vector2? _hoverGridCell;
-    private HashSet<Vector2> _occupiedCell = new();
 
     public override void _Ready() {
         _buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
+        _gridManager = GetNode<GridManager>("GridManager");
+        
         _cursor = GetNode<Sprite2D>("Cursor");
         _placeBuildingButton = GetNode<Button>("PlaceBuildingButton");
-        _highTileMapLayer = GetNode<TileMapLayer>("HighlightTileMapLayer");
-
+        
         _cursor.Visible = false;
 
         _placeBuildingButton.Pressed += OnButtonPressed;
     }
 
     public override void _Process(double delta) {
-        var gridPosition = GetMouseGridCellPosition();
+        var gridPosition = _gridManager.GetMouseGridCellPosition();
         _cursor.GlobalPosition = gridPosition * 64;
 
         if (_cursor.Visible && (!_hoverGridCell.HasValue || _hoverGridCell.Value != gridPosition)) {
             _hoverGridCell = gridPosition;
-            UpdateHighlightTileMapLayer();
+            _gridManager.HighLightValidTilesInRadius(gridPosition, 3);
         }
     }
 
     public override void _UnhandledInput(InputEvent @event) {
         if (_hoverGridCell.HasValue && _cursor.Visible && @event.IsActionPressed("left_click") &&
-            !_occupiedCell.Contains(_hoverGridCell.Value)) {
+            _gridManager.IsTileOccupied(_hoverGridCell.Value)) {
             PlaceBuildingAtHoveredCellPosition();
             _cursor.Visible = false;
         }
@@ -47,31 +50,13 @@ public partial class Main : Node {
         AddChild(building);
 
         building.GlobalPosition = _hoverGridCell.Value * 64;
-        _occupiedCell.Add(_hoverGridCell.Value);
-
+        _gridManager.MarkTileAsOccupied(_hoverGridCell.Value);
+        
         // update tilemap layer to remove highlight
         _hoverGridCell = null;
-        UpdateHighlightTileMapLayer();
+        _gridManager.ClearHighLightTiles();
     }
 
-    private Vector2 GetMouseGridCellPosition() {
-        var mousePos = _highTileMapLayer.GetGlobalMousePosition();
-        var gridPos = mousePos / 64;
-
-        gridPos = gridPos.Floor();
-        return gridPos;
-    }
-
-    private void UpdateHighlightTileMapLayer() {
-        _highTileMapLayer.Clear();
-
-        if (!_hoverGridCell.HasValue)
-            return;
-
-        for (var x = _hoverGridCell.Value.X - 3; x <= _hoverGridCell.Value.X + 3; x++)
-        for (var y = _hoverGridCell.Value.Y - 3; y <= _hoverGridCell.Value.Y + 3; y++)
-            _highTileMapLayer.SetCell(new Vector2I((int)x, (int)y), 0, Vector2I.Zero);
-    }
 
     private void OnButtonPressed() => _cursor.Visible = !_cursor.Visible;
 }
---
+++ scenes/manager/GridManager.cs
@@ -0,0 +1,38 @@
+﻿using Godot;
+
+namespace Game.manager;
+
+public partial class GridManager : Node {
+    [Export] private TileMapLayer _highLightTileMapLayer;
+    [Export] private TileMapLayer _baseTerrainTileMapLayer;
+    
+    private HashSet<Vector2> _occupiedCells = new();
+    
+    public override void _Ready() {
+        
+    }
+
+    public bool IsTileOccupied(Vector2 cell) => !_occupiedCells.Contains(cell);
+    
+    public void MarkTileAsOccupied(Vector2 cell) => _occupiedCells.Add(cell);
+    
+    public void HighLightValidTilesInRadius(Vector2 rootCell, int radius) {
+        ClearHighLightTiles();
+        
+        for (var x = rootCell.X - 3; x <= rootCell.X + 3; x++)
+            for (var y = rootCell.Y - 3; y <= rootCell.Y + 3; y++) {
+                if (!IsTileOccupied(new Vector2(x, y))) continue;
+                _highLightTileMapLayer.SetCell(new Vector2I((int)x, (int)y), 0, Vector2I.Zero);
+            }
+    }
+    
+    public void ClearHighLightTiles() => _highLightTileMapLayer.Clear();
+
+    public Vector2 GetMouseGridCellPosition() {
+        var mouse = _highLightTileMapLayer.GetGlobalMousePosition();
+        var gridPosition = mouse / 64;
+        gridPosition = gridPosition.Floor();
+        return gridPosition;
+    }
+    
+}
