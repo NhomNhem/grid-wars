4491f3fc3dad3ae9b5519d82a884de46d4291a70
NhemNhem <95702943+NhomNhem@users.noreply.github.com>
2026-04-09 08:56:48 +0700

    feat: add GameEvents autoload for building placement events and integrate with GridManager and BuildingComponent

--- scenes/components/BuildingComponent.cs
+++ scenes/components/BuildingComponent.cs
@@ -1,3 +1,4 @@
+using Game.Autoload;
 using Godot;
 
 namespace Game.components;
@@ -8,6 +9,7 @@ public partial class BuildingComponent : Node2D {
 
     public override void _Ready() {
         AddToGroup(nameof(BuildingComponent));
+        GameEvents.EmitBuildingPlaced(this);
     }
     
     public Vector2I GetGridCellPosition() {
--- scenes/manager/GridManager.cs
+++ scenes/manager/GridManager.cs
@@ -1,14 +1,19 @@
-﻿using Game.components;
+﻿using Game.Autoload;
+using Game.components;
 using Godot;
 
 namespace Game.manager;
 
-public partial class GridManager : Node2D {
+public partial class GridManager : Node {
     [Export] private TileMapLayer _highLightTileMapLayer;
     [Export] private TileMapLayer _baseTerrainTileMapLayer;
     
-    private HashSet<Vector2> _occupiedCells = new();
+    private readonly HashSet<Vector2> _occupiedCells = new();
 
+    public override void _Ready() {
+        GameEvents.Instance.BuildingPlaced += OnBuildingPlaced;
+    }
+    
     public bool IsTileOccupied(Vector2I tilePos) {
         var customData = _baseTerrainTileMapLayer.GetCellTileData(tilePos);
         if (customData == null) return false; // treat out of bounds as unoccupied
@@ -37,11 +42,17 @@ public partial class GridManager : Node2D {
     }
     
     private void HighLightValidTilesInRadius(Vector2I rootCell, int radius) {
-        for (var x = rootCell.X - 2; x <= rootCell.X + 2; x++)
-        for (var y = rootCell.Y - 2; y <= rootCell.Y + 2; y++) {
+        for (var x = rootCell.X - radius; x <= rootCell.X + radius; x++)
+        for (var y = rootCell.Y - radius; y <= rootCell.Y + radius; y++) {
             var tilePos = new Vector2I(x, y);
             if (!IsTileOccupied(tilePos)) continue;
             _highLightTileMapLayer.SetCell(tilePos, 0, Vector2I.Zero);
         }
+    }
+    
+    private void OnBuildingPlaced(BuildingComponent buildingComponent) {
+        var gridCell = buildingComponent.GetGridCellPosition();
+        MarkTileAsOccupied(gridCell);
+        HighLightBuildableTiles();
     }
 }
